33e5f51e102abd1b5146e26e0ec5eaa7bf3f3ab6
lirancohen <lirancohen@me.com>
2016-01-17 17:08:01 -0500

    added some comments and defered exit log

--- cmd/main.go
+++ cmd/main.go
@@ -8,32 +8,43 @@ import (
 )
 
 func main() {
+	defer log.Println("Exiting...")
+	//Get the Slack API key from your environment variable
 
+	log.Println("Provisioning bot...")
 	api_key := os.Getenv("SLACKBOT")
+	//Start a New Bot given the API Key and a command indntifier
+	//Command identifier cannot be longer than 3 charachters
 	b, err := bot.New(api_key, ".")
 	if err != nil {
 		log.Println("Not Authenticated, please check your ENV Setting")
 		return
 	}
 
+	//Add Command Handlers, you can also create an array of handlers and loop trhough them
 	b.AddCmdHandler(
 		"test",
 		TestCmdHandler,
 	)
-
 	b.AddCmdHandler(
 		"help",
 		TestCmdHandler2,
 	)
-
+	//Adding general purpose event handlers. These can be used for anything other than a command
+	//example would be keeping track of when a person was last active
 	b.AddEventHandler(
 		"event",
 		TestGeneralHandler,
 	)
+
+	//Turn on logging to see ALL Incoming data
 	b.Logging(true)
+
+	//Run The Bot
 	b.Run()
-	log.Println("Exiting...")
 }
+
+//These are just some example handlers
 
 func TestCmdHandler(e bot.Event, r *bot.Response) {
 	log.Printf("Test Command: %v\n", e)
